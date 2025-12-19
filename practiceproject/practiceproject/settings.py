c9d5e0797f1480428bcd8e98f8c80da1023505f3
Rachanagajam2 <rachanagajam2@gmail.com>
2025-12-19 13:42:41 +0530

    bd connection

--- practiceproject/practiceproject/settings.py
+++ practiceproject/practiceproject/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'practiceproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'django56r',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+        'OPTIONS':{"autocommit":True},
     }
 }
 
--- practiceproject/practiceproject/urls.py
+++ practiceproject/practiceproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from practiceapp.views import first
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('first/',first)
 ]
